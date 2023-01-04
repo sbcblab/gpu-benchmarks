b605704fbd3673002f918f846034c6a2ac0b6de8
leodorna <leo.dorneles@hotmail.com>
2023-01-03 23:18:45 -0300

    fix script

--- create_aux_files.sh
+++ create_aux_files.sh
@@ -5,12 +5,12 @@ dim=$2
 
 cd input_data
 
-./composition_matrix.sh $type $dim composition_$type_$dim.bin
+./composition_matrix.sh $type $dim composition_$1_$2.bin
 
-./rot_matrix.sh $type $dim basic_$1_$dim.bin
+./rot_matrix.sh $type $dim basic_$1_$2.bin
 
-./shift_vector.sh $type $dim 80 basic_$type_$dim.bin
+./shift_vector.sh $type $dim 80 basic_$1_$2.bin
 
-./shift_vector.sh $type $(($dim * 8)) 80 composition_$type_$dim.bin
+./shift_vector.sh $type $(($dim * 8)) 80 composition_$1_$2.bin
 
-./shuffle_vector.sh $dim shuffle_$dim.bin
+./shuffle_vector.sh $dim shuffle_$2.bin
